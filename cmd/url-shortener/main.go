b031507dbd150bc069d8c16493ddabe60857836b
alexandra <Shved1alexandra@gmail.com>
2024-11-04 01:05:54 +0300

    setup logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/netshved/url-shortener/internal/config"
 )
 
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
 func main() {
+	os.Setenv("CONFIG_PATH", "./config/local.yaml")
 	cfg := config.MustLoad()
 
 	fmt.Println(cfg)
 
-	//TODO: init logger: slog (log/slog)
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	//TODO: init storage: sqlite
 
@@ -19,4 +31,21 @@ func main() {
 
 	//TODO: run server
 
+}
+
+func setupLogger(env string) *slog.Logger {
+
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+
+	}
+
+	return log
 }
