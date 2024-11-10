157f0231753daf03f72d6781f759d2e264ca204d
alexandra <Shved1alexandra@gmail.com>
2024-11-10 16:33:40 +0300

    add get method

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -32,7 +32,21 @@ func main() {
 		log.Error("failed to init storage", sl.Err(err))
 		os.Exit(1)
 	}
+	// log.Info("starting storage", slog.String("env", cfg.Env))
 
+	// id, err := storage.SaveURL("https://google.com", "google")
+	// if err != nil {
+	// 	log.Error("failed to save url", sl.Err(err))
+	// 	os.Exit(1)
+	// }
+
+	// log.Info("saved url", slog.Int64("id", id))
+
+	// id, err = storage.SaveURL("https://google.com", "google")
+	// if err != nil {
+	// 	log.Error("failed to save url", sl.Err(err))
+	// 	os.Exit(1)
+	// }
 	_ = storage
 
 	//TODO: init router: chi, (chi render)
--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -2,9 +2,12 @@ package sqlite
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 
+	"github.com/mattn/go-sqlite3"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/netshved/url-shortener/internal/storage"
 )
 
 type Storage struct {
@@ -37,4 +40,52 @@ func New(storagePath string) (*Storage, error) {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
+	const op = "storage.sqlite.SaveURL"
+
+	stmt, err := s.db.Prepare("INSERT INTO url(url, alias) VALUES(?, ?)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	res, err := stmt.Exec(urlToSave, alias)
+	if err != nil {
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrURLExists)
+		}
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: failed to get last insert id: %w", op, err)
+	}
+
+	return id, nil
+}
+
+func (s *Storage) GetURL(alias string) (string, error) {
+	const op = "storage.sqlite.GetURL"
+
+	stmt, err := s.db.Prepare("SELECT url FROM url WHERE alias = ?")
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	var resURL string
+
+	err = stmt.QueryRow(alias).Scan(&resURL)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return resURL, nil
+
 }
